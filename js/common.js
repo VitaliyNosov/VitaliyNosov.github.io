83ccb652fd8abea67b9aca48db6e5b395980d4bd
VitaliyNosov <webdevelorestudio2017@gmail.com>
2024-11-09 10:39:15 +0200

    new version cv

--- js/common.js
+++ js/common.js
@@ -1,156 +1,143 @@
-$(function () {
-
-
-//translator functions
-
-$.html5Translate = function (dict, lang) {
-
-	$('[data-translate-key]').each(function () {
-		$(this).html(dict[lang][$(this).data('translateKey')]);
-	});
-
-};
-
-	
-		var translate = 0;
-
-		$('#btn').on('click', function () {
-			if (translate == 0) {
-				$.html5Translate(dict, 'en');
-
-				translate = 1;
-			}
-			else {
-				$.html5Translate(dict, 'ru');
-
-
-				translate = 0;
-			}
-		});
-
-
-
-
-//media functions
-
-$("#click-left").click(function () {
-		$("#container-left").hide(450);
-		$("#container-right").show(450);
-
-	});
-
-$("#click-right").click(function () {
-		$("#container-left").show(450);
-		$("#container-right").hide(450);
-
-	});
-
-
-// modal popup 
-
-$('.js-button-campaign').click(function () {
-
-    $('.js-overlay-campaign').fadeIn();
-    $('.js-overlay-campaign').addClass('disabled');
-  });
-
-  // закрыть на крестик
-  $('.js-close-campaign').click(function () {
-    $('.js-overlay-campaign').fadeOut();
-
-  });
-
-  // закрыть по клику вне окна
-  $(document).mouseup(function (e) {
-    var popup = $('.js-popup-campaign');
-    if (e.target != popup[0] && popup.has(e.target).length === 0) {
-      $('.js-overlay-campaign').fadeOut();
-
-    }
-  });
-
-
-// my typed js
-
-
-	var typed = new Typed(".hero-typed-text", {
-		strings: ["var mySrc = myImage.getAttribute('src');</br>\ if(mySrc === 'images/firefox-icon.png')</br> \
-      \myImage.setAttribute('src','img/i.png');</br> \ } </br>else {</br> \ myImage.setAttribute('src','img/i.png');</br>\
-    	}\
-			}</br> \ var i = document.querySelector('span');</br>\ var y = document.querySelector('h1');</br>\
-			 var z = document.querySelector('h2');</br>\
-			 var u = document.getElementById('icon')</br>\
-			 var w = document.getElementById('block')"],
-		typeSpeed: 80,//Время набора текста
-		backSpeed: 80,//Время стерания текста
-		loop: true,//Зацыкливает набор текста
-		showCursor: false,
-	});
-
-
-// my body scrolbar
+// jQuery для пользовательского интерфейса и переводчика
 
 $(function () {
-	//The passed argument has to be at least a empty object or a object with your desired options
-	$("body").overlayScrollbars({
-		sizeAutoCapable: false,
-		resize: "vertical"
-	});
-});
-
-$('.block__text').overlayScrollbars({
-    sizeAutoCapable: false,
-    resize: "vertical"
+    // custom color border   
+    $('#block1').click(function() {
+        $('.mobile-color-mob').removeClass('mobile-color-mob');
+    });
     
-});
-
-// GSAP preloader
-
-const text = document.querySelector('.text');
-
-const splitText = (el) => {
-	el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-  return `<div class="word">` +
-			m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
-			`</div>`;
-	});
-	return el;
-};
-
-const split = splitText(text);
-
-function random(min, max){
-  return (Math.random() * (max - min)) + min;
-}
-
-Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
-	TweenMax.from(el, 2.5, {
-		opacity: 0,
-		scale: .1,
-		x: random(-500, 500),
-		y: random(-500, 500),
-		z: random(-500, 500),
-		delay: idx * 0.25,
-		repeat: 0,
-	})
-});
-
-// Появление блока
-
-gsap.to('.preloader', {
-	duration: 2.5,
-	y: 0,
-	scale: 1,
-	opacity: 1
-  })
-
-gsap.to('.linePreloader', {
-	duration: 1,
-	y: 0,
-	scale: 1
-  })
+    $('#block2').click(function() {
+        $('.mob-mod').addClass('mobile-color-mob');
+    });
   
+    // translator functions
+    $.html5Translate = function (dict, lang) {
+        $('[data-translate-key]').each(function () {
+            $(this).html(dict[lang][$(this).data('translateKey')]);
+        });
+    };
   
-});
+    // Устанавливаем начальный язык и состояние кнопки
+    let translate;
+  
+    // Автоматическая установка языка по настройкам браузера
+    function setLanguageByBrowserSettings() {
+        const browserLang = navigator.language || navigator.userLanguage;
+  
+        if (browserLang.startsWith('uk')) {
+            $.html5Translate(dict, 'uk');
+            translate = 0;
+            $('#btn').prop('checked', false);
+        } else {
+            $.html5Translate(dict, 'en');
+            translate = 1;
+            $('#btn').prop('checked', true);
+        }
+    }
+  
+    // Переключение языка при изменении состояния чекбокса
+    $('#btn').on('change', function () {
+        if ($(this).is(':checked')) {
+            $.html5Translate(dict, 'en');
+            translate = 1;
+        } else {
+            $.html5Translate(dict, 'uk');
+            translate = 0;
+        }
+    });
+  
+    // Вызов функции для автоматической установки языка при загрузке страницы
+    $(document).ready(function () {
+        setLanguageByBrowserSettings();
+    });
+  
 
 
+    $("#click-left").click(function () {
+        $("#container-left").hide(450);
+        $("#container-right").show(450);
+    });
+    
+    $("#click-right").click(function () {
+        $("#container-left").show(450);
+        $("#container-right").hide(450);
+    });
+    
+  
+    // modal popup 
+    $('.js-button-campaign').click(function () {
+        $('.js-overlay-campaign').fadeIn();
+        $('.js-overlay-campaign').addClass('disabled');
+    });
+  
+    // Закрыть модальное окно на крестик
+    $('.js-close-campaign').click(function () {
+        $('.js-overlay-campaign').fadeOut();
+    });
+  
+    // Закрыть по клику вне окна
+    $(document).mouseup(function (e) {
+        const popup = $('.js-popup-campaign');
+        if (e.target !== popup[0] && popup.has(e.target).length === 0) {
+            $('.js-overlay-campaign').fadeOut();
+        }
+    });
+  
+    // my typed js
+    var typed = new Typed(".hero-typed-text", {
+        strings: [
+            "var mySrc = myImage.getAttribute('src');</br>\ if(mySrc === 'images/firefox-icon.png')</br> \
+            \myImage.setAttribute('src','img/i.png');</br> \ } </br>else {</br> \ myImage.setAttribute('src','img/i.png');</br>\
+            }\ var i = document.querySelector('span');</br>\ var y = document.querySelector('h1');</br>\ var z = document.querySelector('h2');</br>\ var u = document.getElementById('icon')</br>\ var w = document.getElementById('block')"
+        ],
+        typeSpeed: 80,
+        backSpeed: 80,
+        loop: true,
+        showCursor: false,
+    });
+  
+    // GSAP preloader
+    const text = document.querySelector('.text');
+  
+    const splitText = (el) => {
+        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+            return `<div class="word">` +
+                   m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+                   `</div>`;
+        });
+        return el;
+    };
+  
+    const split = splitText(text);
+  
+    function random(min, max){
+        return (Math.random() * (max - min)) + min;
+    }
+  
+    Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
+        gsap.from(el, 2.5, {
+            opacity: 0,
+            scale: .1,
+            x: random(-500, 500),
+            y: random(-500, 500),
+            z: random(-500, 500),
+            delay: idx * 0.25,
+            repeat: 0,
+        });
+    });
+  
+    // Появление блока
+    gsap.to('.preloader', {
+        duration: 2.5,
+        y: 0,
+        scale: 1,
+        opacity: 1
+    });
+  
+    gsap.to('.linePreloader', {
+        duration: 1,
+        y: 0,
+        scale: 1
+    });
+  });
+  
--- libs/telegram-bot-form-script/telegram-bot-script.js
+++ libs/telegram-bot-form-script/telegram-bot-script.js
@@ -1,47 +1,40 @@
-
 const botToken = '6138840787:AAFMxqaoViiCF22tskdznuhhoLUlBy6cmBw';
 const chatId = '422713968';
 const feedbackForm = document.getElementById('feedbackForm');
+const notyf = new Notyf({
+  position: { x: 'center', y: 'top' },
+  types: [
+    { type: 'success', background: '#034A72', duration: 3000, icon: false },
+    { type: 'error', background: '#f44336', duration: 3000, icon: false }
+  ]
+});
 
 feedbackForm.addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   const name = document.getElementById('nameFF').value;
-   const email = document.getElementById('contactFF').value;
-   const message = document.getElementById('projectFF').value;
-
-   handleFeedbackForm(name, email, message);
- });
-
+  event.preventDefault();
+  const name = document.getElementById('nameFF').value;
+  const email = document.getElementById('contactFF').value;
+  const message = document.getElementById('projectFF').value;
+  handleFeedbackForm(name, email, message);
+});
 
 function sendToTelegram(message) {
-   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-   const data = { chat_id: chatId, text: message };
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+  const data = { chat_id: chatId, text: message };
 
-   axios.post(url, data)
-     .then(response => {
-       console.log('Уведомление успешно отправлено в Telegram');
-       alert('Уведомление успешно отправлено в Telegram');
-     })
-     .catch(error => {
-       console.error('Ошибка отправки уведомления в Telegram:', error);
-       alert('Ошибка отправки уведомления в Telegram:');
-     });
+  axios.post(url, data)
+    .then(response => {
+      console.log('Notification sent successfully');
+      notyf.success('Notification sent successfully');
+    })
+    .catch(error => {
+      console.error('Error sending notification', error);
+      notyf.error('Error sending notification');
+    });
+}
 
- }
-
-
-// function handleFeedbackForm(name, email, message) {
-
-//    const feedbackMessage = `Новая обратная связь:\n\nИмя: ${name}\nEmail: ${email}\nСообщение: ${message}`;
-//    sendToTelegram(feedbackMessage);
-//  }
-
- function handleFeedbackForm(name, email, message) {
+function handleFeedbackForm(name, email, message) {
   const feedbackMessage = `Новая обратная связь:\n\nИмя: ${name}\nEmail: ${email}\nСообщение: ${message}`;
   sendToTelegram(feedbackMessage);
-
-  // Очистить поля формы после отправки
   document.getElementById('nameFF').value = '';
   document.getElementById('contactFF').value = '';
   document.getElementById('projectFF').value = '';
